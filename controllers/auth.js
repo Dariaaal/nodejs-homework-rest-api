23f95fb22289cf433838fd71f395976227727482
Dariaaal <1406dariaal@gmail.com>
2023-06-23 21:00:51 +0300

    done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const {User} = require("../models/user");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const {SECRET_KEY} = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async(req, res) => {
     const {email, password} = req.body;
@@ -15,8 +21,9 @@ const register = async(req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
 
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
     res.status(201).json({
         email: newUser.email,
@@ -32,7 +39,7 @@ const login = async(req, res) => {
         throw HttpError(401, "Email or password invalid");
     }
 
-    const passwordCompare = await bcrypt.compare(password, user.password);
+    const passwordCompare = bcrypt.compare(password, user.password);
 
     if(!passwordCompare) {
         throw HttpError(401, "Email or password invalid");
@@ -73,9 +80,32 @@ const logout = async (req, res, next) => {
     });
 }
 
+const updateAvatar = async (req, res) => {
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+  
+    await User.findByIdAndUpdate(_id, { avatarURL });
+  
+  Jimp.read(`${avatarsDir}/${filename}`,(err, avatar) => {
+    if (err) throw err
+    avatar
+    .resize(250,250)
+    .write(`${avatarsDir}/${filename}`) 
+  })
+  
+    res.json({
+      avatarURL,
+    });
+  };
+
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
     getCurrent: ctrlWrapper(getCurrent),
     logout: ctrlWrapper(logout),
+    updateAvatar: ctrlWrapper(updateAvatar),
 }
