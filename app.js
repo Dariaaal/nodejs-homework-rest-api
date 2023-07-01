2cbace0662e84a2c622e4e1950b7ea05e8a1a9cd
Dariaaal <1406dariaal@gmail.com>
2023-07-02 00:04:25 +0300

    sendgrid

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
-require('dotenv').config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
 
-const contactsRouter = require('./routes/api/contacts')
-const authRouter = require('./routes/api/auth')
+const { SENDGRID_API_KEY } = process.env;
 
-const app = express()
+sgMail.setApiKey(SENDGRID_API_KEY);
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const email = {
+  to: "regam25622@edulena.com",
+  from: "1406dariaal@gmail.com",
+  subject: "Test",
+  html: "<p>Test email</p>",
+};
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-app.use(express.static("public"))
+sgMail.send(email)
+  .then(() => console.log("Email send success"))
+  .catch((error) => console.log(error.message));
 
-app.use('/api/auth', authRouter)
-app.use('/api/contacts', contactsRouter)
+const contactsRouter = require("./routes/api/contacts");
+const authRouter = require("./routes/api/auth");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api/auth", authRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  const {status = 500, message = "Server error"} = err;
-  res.status(status).json({ message })
-})
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
-module.exports = app
-
-
+module.exports = app;
